162c0d67e02f590c5f3aeb26aeb4da4b52d7716c
Luis Pina <luis@luispina.me>
2021-05-17 13:01:44 -0500

    Added scripts to record and replay dacapo experiments

---
+++ artifact/experiments/dacapo-rec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$CHRONICLER_SOURCED" ]; then
+    echo "Environment not set, please run the comment: source env.sh";
+    exit 1
+fi
+
+
+DEBUG="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5555,suspend=n"
+$JAVA_HOME/bin/java $DEBUG -cp $DACAPO_REC_INSTALL:$CHRONICLER_JAR Harness $1
